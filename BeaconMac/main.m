174254397a76a3f3ee1a39790133affbbe4f946c
liyun.dong <liyun.dong@okcoin.com>
2018-08-09 14:33:10 +0800

    init

---
+++ BeaconMac/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  BeaconMac
+//
+//  Created by dongliyun on 2018/8/8.
+//  Copyright © 2018年 dongliyun. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
